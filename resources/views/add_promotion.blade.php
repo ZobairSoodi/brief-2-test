e445aa8e21ec2a4627c90129c9edbc18f49e8518
ZobairSoodi <92028193+ZobairSoodi@users.noreply.github.com>
2022-10-21 09:30:19 +0200

    prototype standard

--- resources/views/add_promotion.blade.php
+++ resources/views/add_promotion.blade.php
@@ -1,15 +1,21 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
-    </head>
-    <body>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}">
+    <title>Laravel</title>
+</head>
+
+<body>
+    <div class="cont">
         <form action="{{ route('insert-promotion') }}" method="POST">
-            {{csrf_field()}}
+            {{ csrf_field() }}
             <label>Nom: <input type="text" name="nom"></label>
             <input type="submit" name="add_promotion">
         </form>
-    </body>
+    </div>
+</body>
+
 </html>
